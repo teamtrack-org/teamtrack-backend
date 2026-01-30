f4db49a5ae219b5710c09a386cff836fbd29b529
buse <elfbusecinar@gmail.com>
2026-01-30 18:11:03 +0300

    chore: initialize Spring Boot project

---
+++ src/main/java/com/teamtrack/teamtrack_backend/TeamtrackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.teamtrack.teamtrack_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamtrackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamtrackBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamtrack/teamtrack_backend/TeamtrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamtrack.teamtrack_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamtrackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
