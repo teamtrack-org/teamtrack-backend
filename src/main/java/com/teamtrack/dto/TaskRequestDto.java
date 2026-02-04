9b33023413db79042b4adfdb11ebda9771affdab
buse <elfbusecinar@gmail.com>
2026-02-04 19:42:28 +0300

    feat: add request validation annotations

--- src/main/java/com/teamtrack/dto/TaskRequestDto.java
+++ src/main/java/com/teamtrack/dto/TaskRequestDto.java
@@ -3,6 +3,7 @@ package com.teamtrack.dto;
 import com.teamtrack.entity.Task;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 public class TaskRequestDto {
 
     @NotBlank(message = "Title cannot be empty")
+    @Size(max = 100, message = "Title cannot exceed 100 characters")
     private String title;
 
     private Task.Status status;
