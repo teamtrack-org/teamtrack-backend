85122ff7a2bdd66a625475d2551b513d972eaefc
buse <elfbusecinar@gmail.com>
2026-02-04 15:09:58 +0300

    feat: add controller layer and update pom configuration

---
+++ src/main/java/com/teamtrack/TeamtrackBackendApplication.java
@@ -0,0 +1,12 @@
+package com.teamtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamtrackBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamtrackBackendApplication.class, args);
+    }
+}
